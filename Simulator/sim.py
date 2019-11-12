5aec66c3781cdea105210e293554eecf96ff2aa4
matt's Laptop <mdaxner@ucsc.edu>
2019-11-12 11:55:16 -0800

    Solved txt writing issues

--- Simulator/sim.py
+++ Simulator/sim.py
@@ -10,6 +10,7 @@ TODO
 import sys 
 import requests
 import lxml.html as lh
+import os, sys, inspect
 
 sys.path.insert(1, '/StockAlgo/Scrapers') 
 
@@ -43,23 +44,39 @@ class Simulator():
 	
 	def buy(self):
 
-		account = open('testfile.txt','w')
+		current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+		parent_dir = os.path.dirname(current_dir)
+		sys.path.insert(0, parent_dir) 
+		
+		file = "\\Data\\accounts.txt"
+		self.path = parent_dir+file
+
+		account = open(self.path,'w')
 		for i in range(0, len(self.Sprice), 1):
 			
-			account.write(str(self.Ssym[i]+str(self.Sprice[i]*self.Svolume[i])))
+			account.write(str(self.Ssym[i]+str(self.Sprice[i])+":"+str(self.Svolume[i])))
 		account.close()
 
 
 	def sell(self):
 		self.load = []
-		with open('testfile.txt', 'r') as account:
+		self.toSell = ['GE']
+		
+		with open(self.path, 'r') as account:
 			line = account.readline()
 			self.load.append(line)
 			while line:
 				line = account.readline()
 				self.load.append(line)
-
+			account.close()
+		with open(self.path ,'r+') as delete:
+			d = delete.readlines()
+			delete.seek(0)
+			for i in d:
+				if i[0:2] != (self.toSell[0])[0:2]:
+					delete.write(i)
+			delete.truncate()
 
 		print(self.load)
 		account.close()
-Simulator()
+Simulator().buy()
