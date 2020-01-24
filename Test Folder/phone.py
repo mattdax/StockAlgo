1651f686b6c5b266ca4039f21962c08c2127a063
mattdax <mjdaxner@gmail.com>
2020-01-24 09:14:05 -0800

    Deleted Garbage Files

--- Test Folder/phone.py
+++
@@ -1,8 +0,0 @@
-
-from textmagic.rest import TextmagicRestClient
-  
-username = "your_textmagic_username"
-token = "your_apiv2_key"
-client = TextmagicRestClient(username, token)
-  
-message = client.messages.create(phones="9990001001", text="Hello TextMagic")
